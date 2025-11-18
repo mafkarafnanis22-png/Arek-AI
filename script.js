dad699a9408982284d555f58feeacf996bbe0061
mafkar afnanis <emailgithublu@example.com>
2025-11-18 10:26:40 +0700

    baru 10

---
+++ script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    document.querySelector(".navbar").style.opacity = "1";
+    document.body.style.overflow = "auto"; // boleh scroll lagi
+}, 7000); // sama dengan durasi intro (7s)
